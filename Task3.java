5c2b1576048fbad46ecfbf337d52dd4431ebc689
DBBEST\Losieva.J <losieva.j@dbbest.com>
2017-01-06 14:14:07 +0200

    task1 update

---
+++ Task3.java
@@ -0,0 +1,27 @@
+/**
+ * Создайте массив из 4 случайных целых чисел из отрезка [10;99], выведите его на экран в строку. Определить и вывести на экран сообщение о том, является ли массив строго возрастающей последовательностью.
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = new int[4];
+        int maxIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int max = array[i];
+            array[i] = (int) ((Math.random() * 90) + 10);
+            System.out.print(array[i] + " ");
+
+                if (array[maxIndex] < array[i]) {
+                    maxIndex = i;
+                }
+
+       }
+
+        System.out.println("Max array element is " + array[maxIndex]);
+
+        if (array[maxIndex] != array[array.length-1]) {
+            System.out.println("Последовательность не является строго возрастающей");
+        }
+    }
+}
+
