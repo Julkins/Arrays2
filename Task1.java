ca311938162efc4b44f414d68d9cac250e9fd8c2
DBBEST\Losieva.J <losieva.j@dbbest.com>
2017-01-06 11:59:24 +0200

    task1 update

--- Task1.java
+++ Task1.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
 import static com.sun.jmx.snmp.ThreadContext.contains;
 
 /**
- * Created by Losieva.J on 12/26/2016.
+ * Создайте массив из 8 случайных целых чисел из отрезка [1;10].
+ * Выведите массив на экран в строку. Замените каждый элемент с нечётным индексом на ноль. Снова выведете массив на экран на отдельной строке.
  */
 public class Task1 {
     public static void main(String[] args) {
 
-         int [] array = new int[10];
+        int[] array = new int[8];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10) + 1;
+
+            if (i % 2 != 0) {
+
+                array[i] = 0;
+
+            }
+            System.out.println(array[i]);
+
+        }
 
     }
-
-    }
+}
 
 
 
