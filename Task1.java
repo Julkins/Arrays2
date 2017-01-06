d59217fd30ef5e0941833d502d81cdf1e3dc41d9
DBBEST\Losieva.J <losieva.j@dbbest.com>
2017-01-06 11:42:35 +0200

    Initial commit

---
+++ Task1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static com.sun.jmx.snmp.ThreadContext.contains;
+
+/**
+ * Created by Losieva.J on 12/26/2016.
+ */
+public class Task1 extends  {
+
+
+    }
+
+
+
+
+
